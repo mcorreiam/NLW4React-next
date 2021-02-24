f620688bbce0342284d901ca760b18735f581769
Marco Correia <m.correia.m@gmail.com>
2021-02-24 19:01:53 -0300

    countdowncomponete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,27 @@
 import { CompletedChallenges } from "../componets/CompletedChallenges";
+import { Countdown } from "../componets/Countdown";
 import { ExperienceBar } from "../componets/ExperienceBar";
 import { Profile } from "../componets/Profile";
 
 import styles from "../styles/pages/Home.module.css"
 
+import Head from 'next/head'
+
 export default function Home() {
   return (
     <div className={styles.container}>
       
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
 
