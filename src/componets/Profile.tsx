0191da8a2bb3df52c73db12a8342ff2eaab761e3
Marco Correia <m.correia.m@gmail.com>
2021-02-24 18:10:54 -0300

    Adicionando imagens e icones no profile

--- src/componets/Profile.tsx
+++ src/componets/Profile.tsx
@@ -6,7 +6,10 @@ export function Profile(){
             <img src="https://github.com/mcorreiam.png" alt="Marco Correia" />
             <div>
                 <strong>Marco Magalhaes</strong>
-                <p>Level 1</p>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level 1
+                </p>
             </div>
         </div>
     )
